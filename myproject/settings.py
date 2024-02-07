1096847613b571765859638e72d095b65ea28580
Md. Fahamidur Rahaman Rafi <44564445+fahamidur@users.noreply.github.com>
2024-02-07 22:58:00 +0600

    adding files for postgresql database

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,11 +76,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": dj_database_url.parse('postgres://django_app_x3pf_user:dalReg0wT9Pk3E4FAT5xxPDjj0uTCOR3@dpg-cn1r1avqd2ns73do44b0-a.oregon-postgres.render.com/django_app_x3pf')
     }
-}
 
 
 # Password validation
